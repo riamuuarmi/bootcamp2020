54f723495c6b00efa595ab33816830ab30e0de86
riamuuarmi <shumailkhan44@gmail.com>
2021-04-21 12:12:44 +0500

    udate ahmed

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and Hello World From Umair.
+          Edit <code>src/App.js</code> and Hello World From Umair Ahmed.
         </p>
         <a
           className="App-link"
